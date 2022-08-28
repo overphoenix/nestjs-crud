828d14f745ccf8e0e6b8b8071bed5ce1a7404b96
overphoenix <hpromatem@protonmail.com>
2022-08-28 16:21:24 +0200

    revert to commonjs

--- packages/util/src/index.ts
+++ packages/util/src/index.ts
@@ -1,9 +1,9 @@
-import { createRequire } from 'node:module';
+// import { createRequire } from 'node:module';
 export * from './checks.util.js';
 export * from './obj.util.js';
 export * from './types/index.js';
 
-const require = createRequire(import.meta.url);
+// const require = createRequire(import.meta.url);
 
 export function safeRequire<T = any>(path: string, loader?: () => T): T | null {
   try {
