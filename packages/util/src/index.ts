df2a599d07ee7b51c448713ba05197980274c2d0
overphoenix <hpromatem@protonmail.com>
2022-08-28 13:04:16 +0200

    dont user require!!!

--- packages/util/src/index.ts
+++ packages/util/src/index.ts
@@ -1,6 +1,9 @@
+import { createRequire } from 'node:module';
 export * from './checks.util.js';
 export * from './obj.util.js';
 export * from './types/index.js';
+
+const require = createRequire(import.meta.url);
 
 export function safeRequire<T = any>(path: string, loader?: () => T): T | null {
   try {
